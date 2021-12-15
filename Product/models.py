91424ea8ea5713b33c28295fa164c913aa4e6a8e
BAD <fathibehrad@gmail.com>
2021-12-15 15:46:15 +0330

    User, Product models updated

--- Product/models.py
+++ Product/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+
 class Product(models.Model):
     pass
 
-class cart(models.Model):
+
+class Cart(models.Model):
     pass
+
 
 class Category(models.Model):
     pass
--- User/models.py
+++ User/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 
+
 class Customer(models.Model):
     pass
+
 
 class Admin(models.Model):
     pass
 
+
 class Profile(models.Model):
     pass
 
+
 class WishList(models.Model):
     pass
+
 
 class Type(models.Model):
     pass
