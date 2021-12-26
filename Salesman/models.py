cd8ac12f0178f98298a463be9efe989a496c9725
BAD <fathibehrad@gmail.com>
2021-12-26 16:52:13 +0330

    models updated

--- Salesman/models.py
+++ Salesman/models.py
@@ -3,7 +3,9 @@ from User.models import Profile
 
 
 class SalesmanProfile(Profile):
-    pass
+    validation = models.FileField()
+    rating = models.FloatField(default=0)
+    registration_date = models.DateField(auto_now_add=True)
 
 
 class Salesman(models.Model):
--- User/models.py
+++ User/models.py
@@ -15,11 +15,18 @@ class Admin(Profile):
 
 
 class Type(models.Model):
+    TYPE = [
+        ("WC", "Wandering_customer"),
+        ("IC", "Impulsive_customer"),
+        ("DC", "Discount_customer"),
+        ("LC", "Loyal_customer"),
+    ]
+    type = models.CharField(max_length=30, choices=TYPE)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
 
 
 class CustomerAddress(models.Model):
     address = models.TextField()
-    postal_code = models.CharField(max_length=10, unique=True)
-
+    postal_code = models.CharField(max_length=10)
+    geographical_location = models.CharField(max_length=50)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
