dd9c3b721a6bb1570fcb594cc8334a1b6af39070
mahdi <syadtym983@gmail.comm>
2022-01-08 12:08:57 +0330

    template

--- Online_Store_Shop/settings.py
+++ Online_Store_Shop/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Online_Store_Shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
