ef68e28e09a16398d873896ddb355843551f7bb7
mahdi <syadtym983@gmail.comm>
2022-02-14 12:27:10 +0330

    dockerize

--- Online_Store_Shop/settings.py
+++ Online_Store_Shop/settings.py
@@ -39,13 +39,7 @@ INSTALLED_APPS = [
     'Product',
     'Salesman',
     'User',
-    'oauth_app',
-    
-    
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
+
 ]
 
 MIDDLEWARE = [
@@ -148,10 +142,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static/'
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static/'
-]
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
