8b8153b4a4cc0e561c60e640d9b1613ee3295971
BAD <fathibehrad@gmail.com>
2022-02-16 00:34:05 +0330

    dockerize

--- News/migrations/0002_initial.py
+++ News/migrations/0002_initial.py
@@ -5,18 +5,18 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
         ('News', '0001_initial'),
-        ('Salesman', '0001_initial'),
+        ('Salesman', '0002_initial'),
     ]
 
     operations = [
         migrations.AddField(
             model_name='news',
             name='salesman',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='Salesman.salesmanprofile'),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE,
+                                    to='Salesman.SalesmanProfile'),
         ),
     ]
