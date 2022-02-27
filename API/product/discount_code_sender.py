c6a94c972400582f6a071bf87d183ada5825d96a
BAD <fathibehrad@gmail.com>
2022-02-27 17:46:44 +0330

    ajab 2 :/

--- API/product/discount_code_sender.py
+++ API/product/discount_code_sender.py
@@ -1,15 +1,16 @@
 import uuid
-from discountcode_app.discount_code.setting import rabbit_connection,pika
+import os
+import pika
 
 
 class DiscountCodeRpcClient(object):
 
     def __init__(self):
-        self.connection = rabbit_connection
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=os.environ.get("RABIT_HOST")))
 
         self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange='discount_code_app',exchange_type='direct')
-
+        self.channel.exchange_declare(exchange='discount_code_app', exchange_type='direct')
 
         result = self.channel.queue_declare(queue='', exclusive=True)
         self.callback_queue = result.method.queue
@@ -23,7 +24,7 @@ class DiscountCodeRpcClient(object):
         if self.corr_id == props.correlation_id:
             self.response = body
 
-    def call(self,user_id_list,expire_time):
+    def call(self, user_id_list, expire_time):
         self.response = None
         self.corr_id = uuid.uuid4()
         self.channel.basic_publish(
@@ -33,7 +34,7 @@ class DiscountCodeRpcClient(object):
                 reply_to=self.callback_queue,
                 correlation_id=self.corr_id,
             ),
-            body={"user_id_list":user_id_list,"expire_time":expire_time})
+            body={"user_id_list": user_id_list, "expire_time": expire_time})
         while self.response is None:
             self.connection.process_data_events()
         return self.response
